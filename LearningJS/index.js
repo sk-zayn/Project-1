4d123cb6a7fce439dcb4e7db14c1d2230a92d0be
Sadik Shaikh <abdullahsadik00@gmail.com>
2025-06-30 21:47:49 +0530

    login form and some basic css

---
+++ LearningJS/index.js
@@ -0,0 +1,30 @@
+let  a = 21;
+if (a <= 10){
+    // console.log("a",a)
+    console.log("true");
+} else{
+    console.log("false")
+}
+//Always save the code before running the code 
+//if the condition is true if block is executed and else block will be colpletely avoided          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
