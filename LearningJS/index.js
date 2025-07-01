4b4c8618d2c7bf09ba2d13214e1a04ee1e9eba58
Sadik Shaikh <abdullahsadik00@gmail.com>
2025-07-02 01:40:14 +0530

    feat: Landing page hero section

--- LearningJS/index.js
+++ LearningJS/index.js
@@ -1,30 +1,59 @@
-let  a = 21;
-if (a <= 10){
-    // console.log("a",a)
-    console.log("true");
-} else{
-    console.log("false")
-}
-//Always save the code before running the code 
-//if the condition is true if block is executed and else block will be colpletely avoided          
+// let a = 11;
+// if (a % 2 == 0){
+//     console.log("Even");
+// } else{
+//     console.log("Odd");
+// }
 
+// let a = 100;
+// let b = 12;
+// let c = 51;
+// if (a < b && a < c){
+//     console.log(a);
+// } else if (b < a && b < c){
+//     console.log(b);
+// } else {
+//     console.log(c);
+// }
 
+// let a = 100;
+// let b = 100;
+// let c = 100;
+// if (a == b && b == c && c == a ){
+//     console.log("Equilateral Triangle");
+// } else if (b==a || b==c || c==a ){
+//     console.log("isoceles triangle");
+// } else {
+//     console.log("Scalene triangle");
+// }
 
+// let i = 1;
+// while (i <= 100){
+//     console.log(i);
+//     i++
+// }
 
+// let ans = 0;
+// let i = 1;
+// while (i<=10) {
+//     ans += i;
+//     i++
+// }
+// console.log(ans);
 
+// for( let i=1; i<=10; i++){
+//     ans+=i;
+// }
+// console.log(ans);
 
+// for(let i=1; i<=25; i++){
+//     if (i % 2 == 0){
+//         console.log(i);
+//     }
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// for(let i=1; i<=25; i++){
+//     if (i % 2 != 0){
+//         console.log(i);
+//     }
+// }
