48b7b4a745d602e036ad0309f464c8ac91134e7c
Sadik Shaikh <abdullahsadik00@gmail.com>
2025-07-07 16:55:03 +0530

    simple landing page

--- LearningJS/index.js
+++ LearningJS/index.js
@@ -57,3 +57,58 @@
 //         console.log(i);
 //     }
 // }
+
+// function isEvenOrOdd (num){
+//     if(num % 2 == 0){
+//         return ("Even");
+//     } else {
+//        return ("Odd")
+//     }
+// }
+
+// let x =121;
+// console.log("X is ", isEvenOrOdd(x));
+
+// for(let i = 1; i <= 50; i++ ){
+//     console.log(i, "is", isEvenOrOdd(i));
+// }
+
+// function isPrime(x) {
+//   for (let i = 2; i <= x - 1; i++) {
+//     if (x % i == 0) {
+//       return false;
+//   }
+//   }
+//   return true;
+// }
+// console.log(isPrime(12));
+
+// function isPrimee (x){
+//     let i=2;
+//     while( i <= x-1 ){
+//         if ( x % i == 0){
+//             return false;
+//         }
+//         i++
+//     }
+//     return true;
+// } 
+// // console.log(isPrimee(12));
+
+// // TODO
+//     // CREATE A ANONYMOUS FUNCTION WHERE YOU'LL BE PASSING A NO=X IN isPrimee() AND BASED ON THAT YOU'LL PRINT A VERBAL RESPONSE
+
+// (function (num){
+//     if (isPrimee(num)){
+//         console.log(num,"is a prime number");
+//     } else {
+//         console.log(num,"is not a prime number");
+//     }
+
+// })(7)
+
+var a = -4;
+let y = -a;
+console.log(a, typeof y);
+console.log(y);
+
